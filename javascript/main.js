e19a8f22b4d92d53942d35220ecdb7bac8d0d1fb
b-husein <bajrektarevic.husein@gmail.com>
2020-06-15 12:00:58 +0200

    initial commit

---
+++ javascript/main.js
@@ -0,0 +1,49 @@
+//Sticky transparent navbar; 
+
+window.addEventListener('scroll', function() {
+    if (window.scrollY > 120) {
+        document.querySelector('#navbar').style.opacity = 0.8;
+    } else {
+        document.querySelector('#navbar').style.opacity = 1;
+    }
+});
+
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top - 70
+        }, 700, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
+
+
+ 
